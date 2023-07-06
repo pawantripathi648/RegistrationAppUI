15a3d1a908862fa62e1c4435f45100c77f6ce39e
pawantripathi648 <pawantripathi648@gmail.com>
2023-07-06 09:56:47 +0530

    login decor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     canActivate: [AuthService],
   },
   {
+    path: 'signup',
+    component: AddEmployeeComponent,
+  },
+  {
     path: 'employee',
     component: EmployeelistComponent,
     canActivate: [AuthService],
